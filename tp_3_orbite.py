39a35a7a03f859e4d3d73a1110ed8c46992ac8ae
Henristote <gicquel.henri@gmail.com>
2025-05-18 14:42:26 +0200

    :sparkles: :zap: Add create_random_circle in tp_3_orbite.py

--- tp_3_orbite.py
+++ tp_3_orbite.py
@@ -3,14 +3,45 @@
 import random
 
 import pygame
-from Mathy import Renderer
+from Mathy import (
+        Renderer,
+        Matrix3x3,
+        TranslationMatrix3x3,
+        RotationMatrix3x3,
+        HomothetyMatrix3x3
+)
+
+
+def generate_random_circle() -> tuple[Matrix3x3, Matrix3x3]:
+    """Generate a set of random circle."""
+    dist = random.randfloat(10, 100)
+    period = random.randfloat(50, 500)
+    theta = float(360/period)
+    pos = TranslationMatrix3x3(dist, 0)
+    rota = RotationMatrix3x3(theta)
+    return [pos, rota]
 
 
 def main():
     """Run the Orbite viewer."""
+
+    circles = [(generate_random_circle()) for _ in range(3)]
+
+    # Display using Renderer
     width, height = 800, 600
     renderer = Renderer(
         width, height,
         "Orbite Viewer",
         bg_color=(255, 255, 255)
     )
+
+    def do_render() -> None:
+
+        renderer.update()
+
+    while renderer.running:
+        do_render()
+
+        renderer.clock.tick(60)
+
+    renderer.quit()

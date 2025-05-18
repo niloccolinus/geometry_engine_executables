6d2f4fa0f7317ad319393b4ea6c19a8e1f79fd75
Leïla P. <leila.karukera@gmail.com>
2025-05-18 20:07:11 +0200

    ✨ Render orbiting circles in tp_3_exo3.py

--- tp_3_exo3.py
+++ tp_3_exo3.py
@@ -1,3 +1,5 @@
+"""Solve problem 3 of math lab no 3 by representing orbiting circles."""
+
 from Mathy import (
     RotationMatrix3x3,
     TranslationMatrix3x3,
@@ -6,44 +8,7 @@ from Mathy import (
     )
 
 
-def rotate_point(x, y, theta, origin=(0, 0)):
-    """
-    Rotate a point around another point in a 2D plane.
-
-    x and y are the point's coordinates.
-    The rotation angle theta should be expressed in degrees, not in radians.
-    """
-    # Express the point's coordinates as a vector3
-    vector = HomogeneousVector3(x, y)
-    # Define the local transform matrix
-    translation = TranslationMatrix3x3(origin[0], origin[1])
-    rotation = RotationMatrix3x3(theta)
-    transform = translation.prod(rotation)
-    # Apply the transform matrix to the point to be rotated
-    new_vector = vector.multiply_by_matrix(transform)
-    # Return the coordinates of the rotated point
-    return new_vector
-
-
-def rotate_circle(center_local, radius, screen_origin, angle, velocity, delta_time, renderer):
-
-    # increment angle with each passing time unit
-    angle += velocity * delta_time
-
-    # generate rotation matrix
-    rot = RotationMatrix3x3(angle)
-
-    # update circle's center coordinates
-    center_world = center_local.multiply_by_matrix(rot)
-    center_screen = center_world.add(screen_origin)
-
-    # draw circle
-    renderer.draw_circle((int(center_screen.x), int(center_screen.y)), radius, color=(255, 0, 0))
-
-    # return new angle, new center world coordinates, new center screen coordinates
-    return angle, center_screen
-
-
+# --- Helper functions ---
 def wp_to_screen(x, y, scale, offset):
     """Convert a 2D point from world coordinates to screen coordinates."""
     a, b = offset
@@ -58,6 +23,24 @@ def wc_to_screen(x, y, scale, offset):
     return world_point
 
 
+def rotate_point(point, theta, origin):
+    """
+    Rotate a point around a local or global origin in a 2D plane.
+
+    Variables point and origin are of type HomogeneousVector3.
+    The rotation angle theta should be expressed in degrees, not in radians.
+    """
+    # Define the local transform matrix
+    translation = TranslationMatrix3x3(origin.x, origin.y)
+    rotation = RotationMatrix3x3(theta)
+    transform = translation.prod(rotation)
+    # Apply the transform matrix to the point to be rotated
+    rotated_point = point.multiply_by_matrix(transform)
+    # Return the coordinates of the rotated point
+    return rotated_point
+
+
+# --- Problem 3 solution ---
 def main():
     """Simulate 2 circles orbiting around another circle."""
     # Define initial values
@@ -76,30 +59,27 @@ def main():
     T = 8  # rotation period in seconds
     omega = 360 / T  # angular velocity
     theta = 8  # rotation angle in degrees
-    local_origin = M
 
     # Values to represent circle C1
     D1 = 10  # distance between center of circle and center of C
     r1 = 10  # radius of circle
-    T1 = 20  # rotation period in seconds
+    T1 = 4  # rotation period in seconds
     omega1 = 360 / T1  # angular velocity
     theta1 = 8  # rotation angle in degrees
-
     # Define the local translation matrix
     translation1 = TranslationMatrix3x3(D1, 0)
-    # Deduce the local coordinates of circle C1's center
+    # Deduce the local coordinates of circle C1's center with M as the origin
     M1 = HomogeneousVector3(M.x, M.y).multiply_by_matrix(translation1)
 
     # Values to represent circle C2
     D2 = 5  # distance between center of circle and center of C
     r2 = 5  # radius of circle
-    T2 = 40  # rotation period in seconds
+    T2 = 2  # rotation period in seconds
     omega2 = 360 / T2  # angular velocity
     theta2 = 8  # rotation angle in degrees
-
     # Define the local translation matrix
     translation2 = TranslationMatrix3x3(D2, 0)
-    # Deduce the local coordinates of circle C2's center
+    # Deduce the local coordinates of circle C2's center with M as the origin
     M2 = HomogeneousVector3(M.x, M.y).multiply_by_matrix(translation2)
 
     # Display using Renderer
@@ -112,7 +92,8 @@ def main():
     )
 
     while renderer.running:
-        dt = renderer.clock.tick(60) / 1000  # milliseconds
+        dt = renderer.clock.tick(60) / 1000  # delta time in milliseconds
+
         renderer.handle_events()
         renderer.clear()
 
@@ -123,7 +104,6 @@ def main():
                 screen_pos = wc_to_screen(x, y, scale, offset)
                 screen_x = screen_pos.x
                 screen_y = screen_pos.y
-
                 if x == 0 or y == 0:  # Draw the x and y axes in black
                     renderer.draw_point(
                         screen_x,
@@ -139,9 +119,29 @@ def main():
                         radius=2
                     )
 
-        theta, local_origin = rotate_circle(M, r, screen_origin, theta, omega, dt, renderer)
-        theta1 = rotate_circle(M1, r1, local_origin, theta1, omega1, dt, renderer)[0]
-        theta2 = rotate_circle(M2, r2, local_origin, theta2, omega2, dt, renderer)[0]
+        # Draw circle C
+        # Calculate the rotation angle
+        theta += omega * dt
+        # Rotate the circle's center
+        m_world = rotate_point(M, theta, origin)  # world coordinates
+        m_screen = m_world.add(screen_origin)  # screen coordinates
+        # Render the circle on-screen
+        renderer.draw_circle((int(m_screen.x), int(m_screen.y)), r,
+                             color=(255, 0, 0))
+
+        # Draw circle C1
+        theta1 += omega1 * dt
+        m1_world = rotate_point(M1, theta1, m_world)
+        m1_screen = m1_world.add(screen_origin)
+        renderer.draw_circle((int(m1_screen.x), int(m1_screen.y)), r1,
+                             color=(0, 255, 0))
+
+        # Draw circle C2
+        theta2 += omega2 * dt
+        m2_world = rotate_point(M2, theta2, m_world)
+        m2_screen = m2_world.add(screen_origin)
+        renderer.draw_circle((int(m2_screen.x), int(m2_screen.y)), r2,
+                             color=(0, 0, 255))
 
         renderer.update()
 

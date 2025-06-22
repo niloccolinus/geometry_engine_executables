e0a306b5b33c98bd3f56a6bfd2b41b85b26e7eb0
Leïla P. <leila.karukera@gmail.com>
2025-06-22 17:54:01 +0200

    🚧 💩 Update tp_5_airplane.py

--- tp_5_airplane.py
+++ tp_5_airplane.py
@@ -16,14 +16,15 @@ def render_cube(cube: Cube,
                 camera: Camera,
                 projection: Projection,
                 renderer: Renderer,
-                color: tuple):
+                color: tuple,
+                angle_deg: float):
     """Apply successive operations to render a 3D cube on a 2D screen."""
-    # cube.transform = cube.transform.__class__()
+    cube.transform = cube.transform.__class__()
 
-    # # Apply rotation on y axis
-    # angle_rad = deg_to_rad(angle_deg)
-    # q = Quaternion.euler_to_quaternion(0, angle_rad, 0)
-    # cube.transform.rotate_quaternion(q)
+    # Apply rotation on y axis
+    angle_rad = deg_to_rad(angle_deg)
+    q = Quaternion.euler_to_quaternion(0, angle_rad, 0)
+    cube.transform.rotate_quaternion(q)
 
     # Apply new transform
     cube_world = cube.renderer.convert_local_to_world(cube)
@@ -73,21 +74,21 @@ def main():
     )
 
     renderer = Renderer(width=800, height=600)
-    # angle_deg = 0
+    angle_deg = 0
 
     while renderer.running:
         renderer.handle_events()
         renderer.clear()
 
-        render_cube(airplane.body, camera, projection, renderer, (0, 0, 0))
-        render_cube(airplane.left_wing, camera, projection, renderer, (0, 255, 0))
-        render_cube(airplane.right_wing, camera, projection, renderer, (0, 0, 255))
+        render_cube(airplane.body, camera, projection, renderer, (0, 0, 0), angle_deg)
+        render_cube(airplane.left_wing, camera, projection, renderer, (0, 255, 0), angle_deg)
+        render_cube(airplane.right_wing, camera, projection, renderer, (0, 0, 255), angle_deg)
 
         renderer.update()
         renderer.clock.tick(60)
 
         # Increment angle
-        # angle_deg = (angle_deg + 1) % 360
+        angle_deg = (angle_deg + 1) % 360
 
 
 if __name__ == "__main__":
